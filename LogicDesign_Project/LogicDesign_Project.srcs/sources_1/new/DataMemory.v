9f3f5274ea1d023f2ea30f01b8dea8a349b81d85
HTH\Hai <hathienhai4@gmail.com>
2024-12-20 21:50:03 +0700

    Update DM.v

--- LogicDesign_Project/LogicDesign_Project.srcs/sources_1/new/DataMemory.v
+++ LogicDesign_Project/LogicDesign_Project.srcs/sources_1/new/DataMemory.v
@@ -23,12 +23,12 @@
 module DataMemory(
     input MemWrite, MemRead,
     input [31:0] Address, WriteData,
-    output [31:0] ReadData
+    output reg [31:0] ReadData
     );
     
     reg [31:0] DMemory [9999:0];
     integer i, baseAddr, offset;
-    wire [31:0] DM_Addr = Address[31:0];
+//    wire [31:0] DM_Addr = Address[31:0];
     initial
     begin
         for (i = 0; i < 10000; i = i + 1)
@@ -39,10 +39,14 @@ module DataMemory(
     
     always @(WriteData or Address or MemRead or MemWrite)
     begin
+        if (MemRead == 1'b1)
+        begin
+            ReadData = DMemory[Address];
+        end
         if (MemWrite == 1'b1)
         begin
-            DMemory[DM_Addr] <= WriteData;
+            DMemory[Address] <= WriteData;
         end
     end
-    assign ReadData = (MemRead == 1'b1) ? DMemory[DM_Addr] : 32'b0;
+//    assign ReadData = (MemRead == 1'b1) ? DMemory[DM_Addr] : 32'b0;
 endmodule
